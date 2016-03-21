02be1010b74fe38b89f421b50be9786f85f4715b
Radley Sustaire <radley@limelightdept.com>
2016-03-21 15:26:27 -0700

    Revamped the pricing engine to use templates instead of filters

--- includes/variations.php
+++ includes/variations.php
@@ -15,6 +15,36 @@ function wcvv_get_step_id( $step_title ) {
 	return sanitize_title_with_dashes($step_title);
 }
 
+// Use custom price templates for virtualized variation products
+function wcvv_custom_price_templates() {
+	if ( has_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price' ) ) {
+		// Archive prices
+		remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price' );
+		add_action( 'woocommerce_after_shop_loop_item_title', 'wcvv_template_loop_price' );
+
+		// Singular prices
+		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price' );
+		add_action( 'woocommerce_single_product_summary', 'wcvv_template_loop_single_price' );
+	}
+}
+add_action( 'init', 'wcvv_custom_price_templates' );
+
+function wcvv_template_loop_price() {
+	if ( wcvv_is_variable( get_the_ID() ) ) {
+		include( WCVV_PATH . '/templates/price-archive.php' );
+	}else{
+		wc_get_template( 'single-product/price.php' );
+	}
+}
+
+function wcvv_template_loop_single_price() {
+	if ( wcvv_is_variable( get_the_ID() ) ) {
+		include( WCVV_PATH . '/templates/price-single.php' );
+	}else{
+		wc_get_template( 'loop/price.php' );
+	}
+}
+
 // Load the customizer for "builder" variations
 function wcvv_render_customizer_wizard() {
 	//$steps = '{"step1":{"title":"Size and Blade Color","options":[{"title":"Option Title","image":"https:\/\/image.png","caption":"This is a caption","price":10,"skip":["step2","step4"]},{"title":"Option Title2","image":"https:\/\/image.png","caption":"This is a caption2","price":0,"skip":[]}]},"step2":{"title":"Another Option","options":[{"title":"Option Title","image":"https:\/\/image.png","caption":"This is a caption","price":0,"skip":[]},{"title":"Option Title2","image":"https:\/\/image.png","caption":"This is a caption2","price":0,"skip":[]}]}}';
@@ -39,17 +69,6 @@ function wcvv_hide_obsolete_fields_from_customizer_products() {
 }
 add_action( 'woocommerce_before_single_product', 'wcvv_hide_obsolete_fields_from_customizer_products' );
 
-
-function wcvv_get_variable_min_price( $post_id ) {
-	$v = get_post_meta( $post_id, 'wcvv-min-price', true );
-	return wcvv_format_price( $v );
-}
-
-function wcvv_get_variable_max_price( $post_id ) {
-	$v = get_post_meta( $post_id, 'wcvv-max-price', true );
-	return wcvv_format_price( $v );
-}
-
 function wcvv_format_price( $v ) {
 	$v = floatval($v);
 	$v = number_format( $v, 2 ); //ceil( $v * 100 ) / 100;
@@ -57,14 +76,28 @@ function wcvv_format_price( $v ) {
 	return $v;
 }
 
+function wcvv_get_added_price( $post_id ) {
+	$min = get_post_meta( $post_id, 'wcvv-price-min', true );
+	$max = get_post_meta( $post_id, 'wcvv-price-max', true );
+
+	if ( $min === null || $min === "" || $min === false ) {
+		wcvv_update_cached_variable_price( $post_id );
+		$min = get_post_meta( $post_id, 'wcvv-price-min', true );
+		$max = get_post_meta( $post_id, 'wcvv-price-max', true );
+	}
+
+	return array( floatval($min), floatval($max) );
+}
 
 // Indicate customizable products cost more than original price
-function wcvv_customizable_product_price_indicator( $price, $product ) {
+/*
+function wcvv_customizable_product_price_indicator( $price, $product, $sale_price = true ) {
 	$price = wc_trim_zeros( $price );
 
 	if( wcvv_is_variable() && !apply_filters( 'wcvv_doing_builder', false ) ) {
-		$variable_product_min = wcvv_get_variable_min_price( get_the_ID() );
-		$variable_product_max = wcvv_get_variable_max_price( get_the_ID() );
+		list ( $min, $max ) = wcvv_get_added_price( $product->id, $sale_price );
+
+		$variable_product_min = floatval($min) + floatval($price);
 
 		if ( is_singular('product') && get_the_ID() === get_queried_object_id() ) {
 			// Single product price
@@ -98,6 +131,68 @@ function wcvv_customizable_product_price_indicator( $price, $product ) {
 	return $price;
 }
 add_filter( 'formatted_woocommerce_price', 'wcvv_customizable_product_price_indicator', 15, 5 );
+*/
+
+
+// Modify the product price outside of the main loop
+/*
+function wcvv_minimum_finished_price( $price, $product ) {
+	if ( is_admin() ) return $price;
+	if ( is_checkout() || is_cart() ) return $price;
+	if ( !wcvv_is_variable( $product->id ) ) return $price;
+
+	// Don't filter the price within the builder
+	if ( apply_filters( 'wcvv_doing_builder', false ) ) return $price;
+
+	list( $min, $max ) = wcvv_update_cached_variable_price( $product->id, false );
+
+	return $price + $min;
+}
+add_filter( 'woocommerce_get_price', 'wcvv_minimum_finished_price', 10, 2 );
+*/
+
+
+function wcvv_update_cached_variable_price( $post_id ) {
+	$steps = wcvv_get_product_steps( $post_id );
+	$step_min_prices = array();
+	$step_max_prices = array();
+	$steps_skip = array();
+
+	foreach( $steps as $step ) {
+		if ( in_array( wcvv_get_step_id($step['variation_title']), $steps_skip, true ) ) continue;
+
+		// Some insane price so that min() works.
+		$lowest_price = 1000000;
+		$highest_price = 0;
+		foreach( $step['options'] as $opt ) {
+			$highest_price = max( $highest_price, floatval($opt['price']) );
+
+			// Skippable fields do not get added for min price
+			if ( $opt['skip'] ) $steps_skip[] = wcvv_get_step_id($opt['skip']);
+			$lowest_price = min( $lowest_price, floatval($opt['price']) );
+		}
+		if ( $lowest_price !== 1000000 && $lowest_price > 0 ) $step_min_prices[] = $lowest_price;
+		if ( $highest_price > 0 ) $step_max_prices[] = $highest_price;
+	}
+
+	$added_min_price = array_sum( $step_min_prices );
+	$added_max_price = array_sum( $step_max_prices );
+
+	if ( !$added_min_price ) $added_min_price = 0;
+	if ( !$added_max_price ) $added_max_price = 0;
+
+	update_post_meta( $post_id, 'wcvv-price-min', $added_min_price );
+	update_post_meta( $post_id, 'wcvv-price-max', $added_max_price );
+}
+
+
+function wcvv_update_cached_price_on_safe( $post_id ) {
+	if ( get_post_type( $post_id ) !== 'product' ) return;
+	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) return;
+
+	wcvv_update_cached_variable_price( $post_id );
+}
+add_action( 'save_post', 'wcvv_update_cached_price_on_safe', 40 );
 
 
 // Replace the "Add to cart" form with a "Build your own" button for customizable products
@@ -114,73 +209,6 @@ function wcvv_customizable_stop_catching_addtocart() {
 	<?php
 }
 add_action( 'woocommerce_before_add_to_cart_form', 'wcvv_customizable_start_catching_addtocart', 20 );
-
-
-// Modify the product price outside of the main loop
-function wcvv_minimum_finished_price( $price, $product ) {
-	if ( is_admin() ) return $price;
-	if ( is_checkout() || is_cart() ) return $price;
-
-	// Don't calculate a price within the builder itself
-	if ( apply_filters( 'wcvv_doing_builder', false ) ) return $price;
-
-	if ( wcvv_is_variable( $product->id ) ) {
-		if ( $v = wcvv_get_variable_min_price( $product->id ) ) {
-			// Price has already been calculated, return it
-			return $v;
-		}else{
-			$steps = wcvv_get_product_steps( $product->id );
-			$step_min_prices = array();
-			$step_max_prices = array();
-			$steps_skip = array();
-
-			foreach( $steps as $step ) {
-				if ( in_array( wcvv_get_step_id($step['variation_title']), $steps_skip, true ) ) continue;
-
-				// Some insane price so that min() works.
-				$lowest_price = 1000000;
-				$highest_price = 0;
-				foreach( $step['options'] as $opt ) {
-					$highest_price = max( $highest_price, floatval($opt['price']) );
-
-					// Skippable fields do not get added for min price
-					if ( $opt['skip'] ) $steps_skip[] = wcvv_get_step_id($opt['skip']);
-					$lowest_price = min( $lowest_price, floatval($opt['price']) );
-				}
-				if ( $lowest_price !== 1000000 && $lowest_price > 0 ) $step_min_prices[] = $lowest_price;
-				if ( $highest_price > 0 ) $step_max_prices[] = $highest_price;
-			}
-
-			$added_min_price = array_sum( $step_min_prices );
-			$added_max_price = array_sum( $step_max_prices );
-
-			// Highest price is not returned, just stored as post meta
-			$max_price = $price + $added_max_price;
-			update_post_meta( $product->id, 'wcvv-max-price', $max_price );
-
-			// Minimum price is returned as the regular price
-			$price = $price + $added_min_price;
-			update_post_meta( $product->id, 'wcvv-min-price', $price );
-		}
-	}
-
-	return $price;
-}
-add_filter( 'woocommerce_get_price', 'wcvv_minimum_finished_price', 10, 2 );
-
-
-//
-function wcvv_clear_cached_variable_price( $post_id ) {
-	if ( get_post_type( $post_id ) !== 'product' ) return;
-	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) return;
-
-	delete_post_meta( $post_id, 'wcvv-min-price' );
-	delete_post_meta( $post_id, 'wcvv-max-price' );
-
-	$product = new WC_Product($post_id);
-	if ( $product && !is_wp_error($product) ) $product->get_price(); // meant to trigger: wcvv_minimum_finished_price()
-}
-add_action( 'save_post', 'wcvv_clear_cached_variable_price', 40 );
 
 
 // Format and validate a list of steps to use for a product
---
+++ templates/price-single.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Loop Price
+ *
+ * This template is based on the original from woocommerce/loop/price.php.
+ *
+ * It is only used for virtualized variation products. It allows us to detect sale price versus regular price.
+ *
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+global $product;
+
+$price_output = '';
+$reg_price_output = '';
+
+// Get prices added by variations, array of min and max combined
+$r = wcvv_get_added_price( $product->id );
+$added_min = $r[0];
+$added_max = $r[1];
+
+// Calculate display of current price
+$price = floatval( $product->get_price() );
+$total_min = $price + $added_min;
+$total_max = $price + $added_max;
+if ( $total_min > 0 || $total_max > 0 ) {
+	$price_output = '$' . wcvv_format_price( $total_min );
+	if ( $total_min !== $total_max ) {
+		$price_output.= ' and up';
+	}
+}else{
+	var_dump('no price?');
+	return;
+}
+
+// Calculate display of regular price
+$reg_price = floatval( $product->get_regular_price() );
+$total_min = $reg_price + $added_min;
+$total_max = $reg_price + $added_max;
+if ( $reg_price !== $price ) {
+	$reg_price_output = '$' . wcvv_format_price( $total_min );
+	if ( $total_min !== $total_max ) {
+		$reg_price_output.= ' and up';
+	}
+}
+
+// -------
+// Output the values
+
+echo '<span class="wcvv-price price">';
+
+if ( $reg_price_output ) {
+	echo '<div><del>';
+	echo '<span class="amount"><span class="label">Regular Price: </span>', $reg_price_output, '</span>';
+	echo '</del></div>';
+}
+
+if ( $price_output ) {
+	if ( $reg_price_output ) echo '<div><ins>';
+	echo '<span class="amount">';
+		if ( $reg_price_output ) echo '<span class="label">Sale Price: </span>';
+		echo $price_output;
+	echo '</span>';
+	if ( $reg_price_output ) echo '</ins></div>';
+}
+
+echo '</span>';
